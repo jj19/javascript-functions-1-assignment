58401fe581bf25d5a73cf5f7c447811e8a1e4ff5
Jose "JJ" Ortiz <jortiz00@gmail.com>
2023-06-23 22:06:29 -0700

    Add changes as directed in the exercises

--- script.js
+++ script.js
@@ -2,4 +2,8 @@
 //numberAdder should take 2 parameters and it should return the sum of those two values
 //After you're done, try looking around at the CSS and HTML to 
 //see if you understand what's going on :)
+let numberAdder = (num1, num2) => {
+  let add = num1 + num2;
 
+  return add;
+}
